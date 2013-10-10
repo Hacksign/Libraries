5d8bb85e7bed5ea8fcbbd52c602d0a1fdf950cb5
hacksign <evilsign@gmail.com>
2013-10-10 17:59:17 +0800

    add iterator support

--- BoostThreadPool/ThreadPool.h
+++ BoostThreadPool/ThreadPool.h
@@ -14,6 +14,48 @@ namespace HUtils{
 	template <typename Thread_T>
 		class ThreadPool{
 			public:
+				typedef boost::shared_ptr<boost::thread> thread_t;
+				typedef std::map<Thread_T*, thread_t> object_thread_t;
+				typedef std::map<boost::thread::id, object_thread_t> threadpool_t;
+				typedef typename threadpool_t::iterator threadpool_iterator_t;
+				template<typename T, typename Item_T>
+					class Iterator_base{
+						public:
+							Iterator_base(T& item) : _item(item), iterator_position(0){
+							}
+							const Iterator_base& operator++(){
+								++iterator_position;
+								return *this;
+							}
+							const Iterator_base& operator--(){
+								--iterator_position;
+								return *this;
+							}
+							Item_T& operator*(){
+								int i = 0;
+								for(typename T::iterator tmp = _item.begin(); tmp != _item.end(); ++tmp){
+									if(i++ != iterator_position) continue;
+									else return *(tmp->first);
+								}
+							}
+							Item_T* operator->(){
+								int i = 0;
+								for(typename T::iterator tmp = _item.begin(); tmp != _item.end(); ++tmp){
+									if(i++ != iterator_position) continue;
+									else return tmp->first;
+								}
+							}
+							bool operator!=(size_t size){
+								if(iterator_position < size) return true;
+								return false;
+							}
+						private:
+							T _item;
+							size_t iterator_position;
+					};
+				typedef Iterator_base<object_thread_t, Thread_T> iterator;
+
+			public:
 				//init an empty pool
 				ThreadPool(){
 					_joiner_running = true;
@@ -34,28 +76,33 @@ namespace HUtils{
 					}
 					_joiner->join();
 				}
-			boost::thread::id add(Thread_T& _thread){
-				for(threadpool_iterator_t it = _thread_pool.begin(); it != _thread_pool.end(); ++it){
-					object_thread_t _each_thread = it->second; 
-					typename object_thread_t::iterator  tmp = _each_thread.begin();
-					if(&_thread == tmp->first) return it->first;
+				boost::thread::id add(Thread_T& _thread){
+					for(threadpool_iterator_t it = _thread_pool.begin(); it != _thread_pool.end(); ++it){
+						object_thread_t _each_thread = it->second; 
+						typename object_thread_t::iterator  tmp = _each_thread.begin();
+						if(&_thread == tmp->first) return it->first;
+					}
+					thread_t _t = thread_t(new boost::thread(boost::ref(_thread)));
+					object_thread_t _obj_trd;
+					_obj_trd[&_thread] = _t;
+					_thread_pool[_t->get_id()] = _obj_trd; 
+					_thread_objects[&_thread] = _t;
+					return _t->get_id();
 				}
-				thread_t _t = thread_t(new boost::thread(boost::ref(_thread)));
-				object_thread_t _obj_trd;
-				_obj_trd[&_thread] = _t;
-				_thread_pool[_t->get_id()] = _obj_trd; 
-				_thread_objects[&_thread] = _t;
-				return _t->get_id();
-			}
-			//get pool size
-			size_t size(){
-				return _thread_pool.size();
-			}
-			bool empey(){
-				return _thread_pool.empty();
-			}
-			public:
-			//iterators
+				//get pool size
+				size_t size(){
+					return _thread_pool.size();
+				}
+				bool empey(){
+					return _thread_pool.empty();
+				}
+				//for iterators
+				iterator begin(){
+					return iterator(_thread_objects);
+				}
+				size_t end(){
+					return _thread_objects.size();
+				}
 			//private functions
 			private:
 				void _join_thread(){
@@ -75,16 +122,14 @@ namespace HUtils{
 						else sleep(5);
 					}
 				}
+			//protected variables
+			protected:
+				object_thread_t _thread_objects;
 			//private variables
 			private:
-				typedef boost::shared_ptr<boost::thread> thread_t;
-				typedef std::map<Thread_T*, thread_t> object_thread_t;
-				typedef std::map<boost::thread::id, object_thread_t> threadpool_t;
-				typedef typename threadpool_t::iterator threadpool_iterator_t;
 				threadpool_t _thread_pool;
 				boost::shared_ptr<boost::thread> _joiner;
 				bool _joiner_running;
-				object_thread_t _thread_objects;
 		};
 }
 #endif
--- BoostThreadPool/usage.cpp
+++ BoostThreadPool/usage.cpp
@@ -2,29 +2,34 @@
 #include "ThreadPool.h"
 #include <unistd.h>
 
+//use below command to compile me:
+//	g++ usage.cpp -o test -lboost_thread -lboost_system
+
 using namespace HUtils;
 using namespace std;
 
 class MyThreadClass : public IThread{
 	public:
-		MyThreadClass(int i) : number(i){};
+		MyThreadClass(int i) : number(i), thread_id(number){};
 		virtual void operator()(){
 			++number;
 		}
+		void print(){
+			cout<<"id:"<<thread_id<<endl;
+		}
 	public:
 		int number;
+		int thread_id;
 };
 
 int main(){
 	MyThreadClass t1(1);
 	ThreadPool<MyThreadClass> thread_pool(t1);
-	//cout<<"pool size:"<<thread_pool.size()<<endl;
 	MyThreadClass t2(2);
 	thread_pool.add(t2);
-	//cout<<"pool size:"<<thread_pool.size()<<endl;
-	sleep(3);
-	//cout<<"pool size:"<<thread_pool.size()<<endl;
-	cout<<t1.number<<endl;
-	cout<<t2.number<<endl;
+	for(ThreadPool<MyThreadClass>::iterator it = thread_pool.begin(); it != thread_pool.end(); ++it){
+		it->print();
+	}
+	sleep(1);//wait all done
 	return 0;
 }
